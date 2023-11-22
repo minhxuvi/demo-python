2cde669d75098101e73cc117b435eb0fca8bc220
Mốc Meo <minhxuvi@gmail.com>
2023-11-22 09:11:26 +0700

    Add examples and explanations for various programming concepts

--- code.py
+++ code.py
@@ -129,5 +129,201 @@ print(name)  # Output: John
 print(age)  # Output: 20
 print(major)  # Output: Computer Science
 
+# Function example
+def add(x, y):
+    return x + y
+
+# Lambda function example
+add = lambda x, y: x + y
+
+# Dictionary comprehension example
+numbers = [1, 2, 3, 4, 5]
+squared_numbers = {num: num ** 2 for num in numbers}
+print(squared_numbers)  # Output: {1: 1, 2: 4, 3: 9, 4: 16, 5: 25}
+
+# Unpacking example
+numbers = [1, 2, 3, 4, 5]
+x, y, *rest = numbers
+print(x)  # Output: 1
+print(y)  # Output: 2
+print(rest)  # Output: [3, 4, 5]
+
+# Unpacking keyword arguments example
+def func(*args, **kwargs):
+    print(args)
+    print(kwargs)
+
+# Object-oriented programming example
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def greet(self):
+        print(f"Hello, my name is {self.name}.")
+
+# Magic methods example
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return f"Person({self.name})"
+
+    def __repr__(self):
+        return f"Person({self.name})"
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def __add__(self, other):
+        return Person(self.name + other.name)
+    
+# Class method example vs static method example
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    @classmethod
+    def create(cls, name):
+        return cls(name)
+
+    @staticmethod
+    def create(name):
+        return Person(name)
+
+# Class inheritance example vs composition example
+# Inheritance
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def greet(self):
+        print(f"Hello, my name is {self.name}.")
+
+class Student(Person):
+    def __init__(self, name, major):
+        super().__init__(name)
+        self.major = major
+
+    def study(self):
+        print(f"I am studying {self.major}.")
+
+# Composition
+class AnotherPerson:
+    def __init__(self, name):
+        self.person = Person(name)
+
+    def greet(self):
+        self.person.greet()
+
+# Type hinting example
+def add(x: int, y: int) -> int:
+    return x + y
+
+# Error handling example
+try:
+    x = 10 / 0
+except ZeroDivisionError:
+    print("Cannot divide by zero.")
+else:
+    print("Division successful.")
+finally:
+    print("This is always executed.")
+
+
+# File handling example
+# Write to file
+with open("data.txt", "w") as file:
+    file.write("Hello, world!")
+
+# Read from file
+with open("data.txt", "r") as file:
+    data = file.read()
+    print(data)  # Output: Hello, world!
+
+# Importing module example
+# Path: demo-python/main.py
+import code
+
+print(code.add(1, 2))  # Output: 3
+
+# Custom error example
+class CustomError(Exception):
+    pass
+# Raise custom error
+raise CustomError("This is a custom error.")
+
+# First class function example
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def apply(func, x, y):
+    return func(x, y)
+
+
+print(apply(add, 1, 2))  # Output: 3
+print(apply(subtract, 1, 2))  # Output: -1
+print(apply(multiply, 1, 2))  # Output: 2
+
+# Simple decorator example
+def decorator(func):
+    def wrapper():
+        print("Before function.")
+        func()
+        print("After function.")
+    return wrapper
+
+# at sign (@) syntax
+@decorator
+def hello():
+    print("Hello, world!")
+
+hello()  # Output: Before function. Hello, world! After function.
+
+# Functools wraps example
+from functools import wraps
+
+def decorator(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print("Before function.")
+        func(*args, **kwargs)
+        print("After function.")
+    return wrapper
+
+@decorator
+def hello():
+    print("Hello, world!")
+
+print(hello.__name__)  # Output: hello
+
+# Do not use mutable objects as default arguments
+def add(x, y=[]):
+    y.append(x)
+    return y
+
+print(add(1))  # Output: [1]
+print(add(2))  # Output: [1, 2]
+print(add(3))  # Output: [1, 2, 3]
+
+# Use None as default argument instead
+def add(x, y=None):
+    if y is None:
+        y = []
+    y.append(x)
+    return y
+
+print(add(1))  # Output: [1]
+print(add(2))  # Output: [2]
+print(add(3))  # Output: [3]
 
 
